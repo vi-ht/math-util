4c312b4f7fead9a82f743a2bc0a1d94c8a30b2fd
vi-ht <huynhthanhvi1102@gmail.com>
2020-11-17 15:50:49 +0700

    add lib

---
+++ test/vihtt/test/MathTest.java
@@ -0,0 +1,21 @@
+package vihtt.test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Thanh Vi
+ */
+public class MathTest {
+      
+      public MathTest() {
+      }
+      
+}
